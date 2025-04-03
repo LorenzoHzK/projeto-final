a9f03ff4d98031b5e8f7581b5e212e185278d8f0
LorenzoHzK <matheuslorenzosiquiera@gmail.com>
2025-04-03 08:34:42 -0300

    Salvando os arquivos modificados ate o memomento

--- docker-laravel/src/database/migrations/2025_04_02_092308_categories.php
+++ docker-laravel/src/database/migrations/2025_04_02_092308_categories.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create ('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
         });
     }
 

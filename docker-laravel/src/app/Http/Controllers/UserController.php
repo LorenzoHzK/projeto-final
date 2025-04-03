e526cd1eaa18ad1987ab4ae4fbe837654107514d
LorenzoHzK <matheuslorenzosiquiera@gmail.com>
2025-04-03 17:32:30 -0300

    movendo autenticacao

--- docker-laravel/src/app/Http/Controllers/UserController.php
+++ docker-laravel/src/app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -11,38 +14,57 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return response()->json($users);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function register(Request $request)
     {
-        //
+            $request->validate([
+            "email" => "required|string|email|unique:users",
+            "name" => "required|string|min:3",
+            "password" => "required|string|min:8"
+        ]);
+
+        $user = User::create([
+            "email" => $request->email,
+            "name" => $request->name,
+            "password" => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            "message" => "User created successfully",
+            "user" => $user
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth :: attempt($credentials)) {
+            $user = $request->user();
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                "access_token" => $token,
+                "token_type" => "Bearer"
+            ]);
+        }
+
+        return response()->json([
+            "message" => "Invalid email or password"
+        ]);
     }
 }
--- docker-laravel/src/routes/api.php
+++ docker-laravel/src/routes/api.php
@@ -3,51 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
-use App\Http\AddressController;
-use App\Http\OrdersController;
+use App\Http\Controllers\UserController;
 
-Route::resource('/address', AddressController::class);
-Route::resource('/orders', OrdersController::class);
+//Rotas para o login e registro do usuário
 
+Route::post('/register', [UserController::class, 'register']);
 
-Route::post("/register", function(Request $request){
-    $request->validate([
-        "email" => "required|string|email|unique:users",
-        "name" => "required|string|min:3",
-        "password" => "required|string|min:8"
-    ]);
-
-    $user = User::create([
-        "email" => $request->email,
-        "name" => $request->name,
-        "password" => Hash::make($request->password)
-    ]);
-
-    return response()->json([
-        "message" => "User created successfully",
-        "user" => $user
-    ]);
-});
-
-Route::post('/login', function (Request $request) {
-    $credentials = $request->only('email', 'password');
-
-    if (Auth :: attempt($credentials)) {
-        $user = $request->user();
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            "acess_token" => $token,
-            "token_type" => "Bearer"
-        ]);
-    }
-
-    return response()->json([
-        "message" => "Invalid email or password"
-    ]);
-
-});
+Route::post('/login', [UserController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user/profile', function (Request $request) {
     return $request->user();

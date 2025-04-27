e81013da8aa92f672b3a266164817d39ec7efdcd
LorenzoHzK <matheuslorenzosiquiera@gmail.com>
2025-04-26 22:09:33 -0300

    Rotas alteradas com prefix

--- docker-laravel/src/routes/api.php
+++ docker-laravel/src/routes/api.php
@@ -21,66 +21,80 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/verify/token', [UserController::class, 'verifyToken']);
 
     //Route to User
-    Route::get('/user/me', [UserController::class, 'info_user']);
-    Route::put('/user/me', [UserController::class, 'update_user']);
-    Route::delete('/user/me', [UserController::class, 'delete_user']);
-    Route::post('/user/create/moderator', [UserController::class, 'create_moderator']);
-
+    Route::prefix('user')->group(function () {
+    Route::get('/me', [UserController::class, 'info_user']);
+    Route::put('/me', [UserController::class, 'update_user']);
+    Route::delete('/me', [UserController::class, 'delete_user']);
+    Route::post('/create/moderator', [UserController::class, 'create_moderator']);
+    });
 
     // Route to address
-    Route::get('/address/{id?}', [AddressController::class, 'showAddress']);
-    Route::post('/address', [AddressController::class, 'createAddress']);
-    Route::delete('/address/{id}', [AddressController::class, 'deleteAddress']);
-    Route::put('/address/{id}', [AddressController::class, 'updateAddress']);
+    Route::prefix('address')->group(function () {
+    Route::get('/{id?}', [AddressController::class, 'showAddress']);
+    Route::post('/', [AddressController::class, 'createAddress']);
+    Route::delete('/{id}', [AddressController::class, 'deleteAddress']);
+    Route::put('/{id}', [AddressController::class, 'updateAddress']);
+    });
 
     // Route for Categories
-    Route::get('/category/{id?}', [CategoriesController::class, 'showCategories'])->withoutMiddleware(['auth:sanctum']);
-    Route::post('/category', [CategoriesController::class, 'createCategories']);
-    Route::delete('/category/{id}', [CategoriesController::class, 'deleteCategory']);
-    Route::put('/category/{id}', [CategoriesController::class, 'updateCategory']);
-    Route::get('/category/user/{user_id}', [CategoriesController::class, 'categoriesByUser']);
+    Route::prefix('category')->group(function () {
+    Route::get('/{id?}', [CategoriesController::class, 'showCategories'])->withoutMiddleware(['auth:sanctum']);
+    Route::post('/', [CategoriesController::class, 'createCategories']);
+    Route::delete('/{id}', [CategoriesController::class, 'deleteCategory']);
+    Route::put('/{id}', [CategoriesController::class, 'updateCategory']);
+    Route::get('/user/{user_id}', [CategoriesController::class, 'categoriesByUser']);
+    });
 
     // Route to Coupon
-    Route::get('/coupon/{id?}', [CouponsController::class, 'showCoupons']);
-    Route::post('/coupon', [CouponsController::class, 'createCoupons']);
-    Route::delete('/coupon/{id}', [CouponsController::class, 'deleteCoupons']);
-    Route::put('/coupon/{id}', [CouponsController::class, 'updateCoupons']);
+    Route::prefix('coupon')->group(function () {
+    Route::get('/{id?}', [CouponsController::class, 'showCoupons']);
+    Route::post('/', [CouponsController::class, 'createCoupons']);
+    Route::delete('/{id}', [CouponsController::class, 'deleteCoupons']);
+    Route::put('/{id}', [CouponsController::class, 'updateCoupons']);
+    });
 
     // Route to Discounts
-    Route::get('/discount/{id?}', [DiscountController::class, 'showDiscount']);
-    Route::post('/discount', [DiscountController::class, 'createDiscount']);
-    Route::delete('/discount/{id}', [DiscountController::class, 'deleteDiscount']);
-    Route::put('/discount/{id}', [DiscountController::class, 'updateDiscount']);
+    Route::prefix('discount')->group(function () {
+    Route::get('/{id?}', [DiscountController::class, 'showDiscount']);
+    Route::post('/', [DiscountController::class, 'createDiscount']);
+    Route::delete('/{id}', [DiscountController::class, 'deleteDiscount']);
+    Route::put('/{id}', [DiscountController::class, 'updateDiscount']);
+    });
 
     //Route to Products
     Route::post('/image/product/{product_id}', [ProductsController::class, 'uploadImage']);
     Route::get('/image/product/{product_id}', [ProductsController::class, 'showImage']);
 
-    Route::get('/product/{id?}', [ProductsController::class, 'showProducts']);
-    Route::get('/product/category/{category_id}', [ProductsController::class, 'productsByCategory']);
-    Route::post('/product', [ProductsController::class, 'createProducts']);
-    Route::put('/product/{id?}', [ProductsController::class, 'updateProducts']);
-    Route::delete('/product/{id?}', [ProductsController::class, 'deleteProducts']);
-    Route::delete('/product/{id?}/stock', [ProductsController::class, 'updateStock']);
+    Route::prefix('product')->group(function () {
+    Route::get('/{id?}', [ProductsController::class, 'showProducts']);
+    Route::get('/category/{category_id}', [ProductsController::class, 'productsByCategory']);
+    Route::post('/', [ProductsController::class, 'createProducts']);
+    Route::put('/{id?}', [ProductsController::class, 'updateProducts']);
+    Route::delete('/{id?}', [ProductsController::class, 'deleteProducts']);
+    Route::delete('/{id?}/stock', [ProductsController::class, 'updateStock']);
+    });
 
 // Route to Cart
-    Route::get('/cart/', [CartsController::class, 'showCart']);
-    Route::post('/cart/', [CartsController::class, 'createCart']);
+    Route::prefix('cart')->group(function () {
+    Route::get('/', [CartsController::class, 'showCart']);
+    Route::post('/', [CartsController::class, 'createCart']);
 
 // Route to CartItem
-    Route::get('/cart/items', [CartItemController::class, 'cartItems']);
-    Route::put('/cart/items', [CartItemController::class, 'updateCartItem']);
-    Route::post('/cart/items', [CartItemController::class, 'createCartItem']);
-    Route::delete('/cart/items', [CartItemController::class, 'deleteCartItem']);
+    Route::get('/items', [CartItemController::class, 'cartItems']);
+    Route::put('/items', [CartItemController::class, 'updateCartItem']);
+    Route::post('/items', [CartItemController::class, 'createCartItem']);
+    Route::delete('/items', [CartItemController::class, 'deleteCartItem']);
 
 // Route to clean the CartItem
-    Route::delete('/cart/clear', [CartsController::class, 'clearCartItem']);
-
-//Route to orders
-    Route::get('/orders', [OrdersController::class, 'showOrders']);
-    Route::post('/orders', [OrdersController::class, 'createOrders']);
-    Route::get('/orders/{order_id}', [OrdersController::class, 'specificOrders']);
-    Route::put('/orders/{order_id}', [OrdersController::class, 'updateOrders']);
-    Route::delete('/orders/{id}', [OrdersController::class, 'deleteOrders']);
+    Route::delete('/clear', [CartsController::class, 'clearCartItem']);
 });
 
+//Route to orders
+    Route::prefix('order')->group(function () {
+    Route::get('/', [OrdersController::class, 'showOrders']);
+    Route::post('/', [OrdersController::class, 'createOrders']);
+    Route::get('/{order_id}', [OrdersController::class, 'specificOrders']);
+    Route::put('/{order_id}', [OrdersController::class, 'updateOrders']);
+    Route::delete('/{id}', [OrdersController::class, 'deleteOrders']);
+    });
+});
